1965943d689bbe978863b31832d10266e0871c45
rodrigo <rodrigocsilva63@gmail.com>
2022-09-13 22:34:36 -0300

    add routes

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,13 @@ import express from 'express';
 
 const app = express();
 
-app.get('/ads', (req, res) =>{
+//Games controller
+
+app.get('/games', (req, res) => {
+    return res.status(200).json();
+})
+
+app.get('/games/:id/ads', (req, res) =>{
     return res.json([
         {id: 1, name: "ads 1"},
         {id: 2, name: "ads 2"},
@@ -10,5 +16,20 @@ app.get('/ads', (req, res) =>{
         {id: 4, name: "ads 4"},
     ]);
 });
+
+//Ads controller
+
+app.get('/ads', (req, res) =>{
+    return res.json();
+})
+
+app.post('/ads', (req, res) => {
+    return res.status(201).json()
+})
+
+app.get('/ads/:id/discord', (req, res) => {
+    const adsId = req.params.id;
+    return res.json();
+})
 
 app.listen(3333);
